ed0166414c645d7d8fbf4daca429b74cc8489ab2
Ypur Neme <you@example.com>
2025-12-04 10:56:17 +0900

    Add php-practice_2.php

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,181 @@
 <?php
 // Q1 tic-tac問題
 
+for ($i = 1; $i <= 100; $i++) {
+    
+    if ($i % 20 === 0) {
+        echo 'tic-tac';
+    }
+    
+    elseif ($i % 4 === 0) {
+        echo 'tic';
+    }
+    
+    elseif ($i % 5 === 0) {
+        echo 'tac';
+    }
+    
+    else echo $i;
+
+    echo PHP_EOL;
+}
+
 
 // Q2 多次元連想配列
+
+//問題1
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+    ];
+    
+    echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+
+
+    //問題2
+    $personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+    ];
+    
+    
+$number = 1;
+
+foreach ($personalInfos as $info) {
+    echo $number . '番目の' . $info['name'] . 'のメールアドレスは' . $info['mail'] .
+        'で、電話番号は' . $info['tel'] . "です。\n";
+    $number++;
+}
+
+
+//問題3
+
+
+
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
+
+$ageList = [25, 30, 18];
+
+
+foreach ($personalInfos as $key => $info) {
+    $personalInfos[$key]['age'] = $ageList[$key];
+}
+
+var_dump($personalInfos);
 
 
 // Q3 オブジェクト-1
 
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+
+$student = new Student(120, '山田');
+
+
+echo '学籍番号' . $student->studen
+
 
 // Q4 オブジェクト-2
 
+<?php
+
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend($subject)
+    {
+        echo $this->studentName . 'は' . $subject . 'の授業に参加しました。学籍番号：' . $this->studentId;
+    }
+}
+
+$yamada = new Student(120, '山田');
+$yamada->attend('PHP');
+
+
+
 
 // Q5 定義済みクラス
+
+//問題1
+$date = new DateTime();
+
+$date->modify('-1 month');
+
+echo $date->format('Y-m-d');
+
+//問題2
+
+$today = new DateTime();
+
+$past = new DateTime('1992-04-25');
+
+$diff = $today->diff($past);
+
+echo 'あの日から' . $diff->days . '日経過しました。';
+
 ?>
